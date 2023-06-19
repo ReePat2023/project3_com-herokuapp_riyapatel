bdccfa5625241743f6b4716ba3844b5541246cfa
reepat2023 <reepatel2023@gmail.com>
2023-06-20 02:46:34 +0530

    project3_com-herokuapp_riyapatel

---
+++ src/project3_riyapatel/MultiBrowserTest3.java
@@ -0,0 +1,53 @@
+package project3_riyapatel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MultiBrowserTest3 {
+    static String browser = "Firefox";
+    static String baseUrl = "http://the-internet.herokuapp.com/login";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("Chrome")){
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("Firefox")){
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        }
+
+        //1. Setup Chrome browser
+        WebDriver driver = new FirefoxDriver();
+
+        //2. Open the URL into Browser
+        String baseUrl = "http://the-internet.herokuapp.com/login";
+        driver.get(baseUrl);
+
+        //3. Print the title of the page
+        String title = driver.getTitle();
+        System.out.println("Page Title is " + title);
+
+        //4. Print the current url
+        String url = driver.getCurrentUrl();
+        System.out.println("Current url is " + url);
+
+        //5. Print the page source
+        System.out.println("Page source " + driver.getPageSource());
+
+        //6. Enter the email to email field
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("riya123");
+
+        //7. Enter the password to password field
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("riya123");
+
+        //8. Close the browser
+        driver.close();
+    }
+}
